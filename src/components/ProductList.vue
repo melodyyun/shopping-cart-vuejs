3cb8acb0203c9d11f7b7fe3e29d9ff4003669fce
Melody Yun Chen <Melodyyunc@gmail.com>
2018-07-19 21:39:19 -0400

    render products from store

---
+++ src/components/ProductList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Products List</h1>
+    <ul>
+      <li v-for="product in products">{{product.title}} - {{product.price}}</li>
+    </ul>
+  </div>
+</template>
+
+<script> 
+  import shop from '@/api/shop'
+  export default {
+    data() {
+      return{
+        products: []
+      }
+    },
+
+    created() {
+      shop.getProducts(products => {
+        this.products = products;
+        console.log(products);
+      });
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
